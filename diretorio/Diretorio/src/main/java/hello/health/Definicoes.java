a2fa27df55c6345f870cd178c6ab3b1edc5e49cc
josepferreira <zepedroferreira11@gmail.com>
2019-05-19 22:17:01 +0100

    Alterei só o gitignore

--- diretorio/Diretorio/src/main/java/hello/health/Definicoes.java
+++ diretorio/Diretorio/src/main/java/hello/health/Definicoes.java
@@ -1,6 +1,6 @@
 package hello.health;
 
 public class Definicoes {
-    public static String ZKIP = "172.19.0.4";
+    public static String ZKIP = "172.18.0.2";
     public static String ZKPort = "2181";
 }
